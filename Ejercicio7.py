122965702b4ca07aa64c2b941107c8c844e71327
Mark.os <mruizlopez5@educacion.navarra.es>
2024-10-21 18:36:08 +0200

    Create Ejercicio7.py

---
+++ Ejercicio7.py
@@ -0,0 +1,9 @@
+"""Escribir un programa que pida al usuario su peso (en kg) y estatura (en metros), 
+calcule el índice de masa corporal y lo almacene en una variable, 
+y muestre por pantalla la frase Tu índice de masa corporal es <imc> donde <imc> 
+es el índice de masa corporal calculado redondeado con dos decimales."""
+
+kg = float(input("introduce tu peso en kg\n"))
+m = float(input("introduce tu altura en metros\n"))
+imc = kg/m
+print("Tu índice de masa corporal es "+ str(round(imc,2)))
