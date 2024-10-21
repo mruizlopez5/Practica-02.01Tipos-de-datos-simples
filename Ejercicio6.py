8bbdcee929ae4c34665b707a05154e63128684b9
Mark.os <mruizlopez5@educacion.navarra.es>
2024-10-21 18:21:41 +0200

    Create Ejercicio6.py

---
+++ Ejercicio6.py
@@ -0,0 +1,5 @@
+"""Escribir un programa que lea un entero positivo, , introducido por el usuario y 
+después muestre en pantalla la suma de todos los enteros desde 1 hasta dicho número"""
+
+n = int(input("se calculara el sumatorio de 1 hasta el numero introducido (introducelo xd)\n"))
+print("El sumatorio de 1 hasta "+str(n)+"es igual a: "+str((n*(n+1))/2))
