42277dfb6c00e575ffc7f793ddb6c60160ea81ad
Mark.os <mruizlopez5@educacion.navarra.es>
2024-10-21 18:58:05 +0200

    Create Ejercicio8.py

---
+++ Ejercicio8.py
@@ -0,0 +1,12 @@
+"""Escribir un programa que pida al usuario dos números enteros y 
+muestre por pantalla la <n> entre <m> da un cociente <c> y un resto 
+<r> donde <n> y <m> son los números introducidos por el usuario, y 
+<c> y <r> son el cociente y el resto de la división entera respectivamente."""
+
+num = int(input("introduce un numerador"))
+den = int(input("introduce un denominador"))
+ 
+resto = num%den
+cocido = num//den
+
+print("el número " + str(num) + " entre " + str(den) + " da un cociente "+ str(cocido) + " y un resto " + str(resto))
