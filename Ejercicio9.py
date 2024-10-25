f38def883f8500c61e62c940cfe6f26fa539a462
Mark.os <mruizlopez5@educacion.navarra.es>
2024-10-25 19:16:25 +0200

    Create Ejercicio9.py

---
+++ Ejercicio9.py
@@ -0,0 +1,14 @@
+"""Escribir un programa que pregunte al usuario 
+una cantidad a invertir, el interés anual y el 
+número de años, y muestre por pantalla el capital
+obtenido en la inversión"""
+
+print("welcome to la calculadora de inversiones\n")
+
+inversion = float(input("Introduzca la cantidad a invertir\n"))
+interes = float(input("introduce el valor del interés anual\n"))
+años = int(input("introduce el numero de años que estrá en inversion\n"))
+
+total = inversion*pow((1+interes),años)
+
+print("has capitalizado un capital total de: ",str(total)," capitales")
