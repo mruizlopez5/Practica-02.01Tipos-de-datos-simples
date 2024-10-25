f82dcb53d7a51b796c0511614b59fe8af2743eed
Mark.os <mruizlopez5@educacion.navarra.es>
2024-10-25 18:57:46 +0200

    Update Ejercicio8.py
    corregido interlineado

--- Ejercicio8.py
+++ Ejercicio8.py
@@ -3,8 +3,8 @@ muestre por pantalla la <n> entre <m> da un cociente <c> y un resto
 <r> donde <n> y <m> son los números introducidos por el usuario, y 
 <c> y <r> son el cociente y el resto de la división entera respectivamente."""
 
-num = int(input("introduce un numerador"))
-den = int(input("introduce un denominador"))
+num = int(input("introduce un numerador\n"))
+den = int(input("introduce un denominador\n"))
  
 resto = num%den
 cocido = num//den
